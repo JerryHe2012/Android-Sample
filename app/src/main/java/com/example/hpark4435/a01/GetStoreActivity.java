a8ca9d4789d404210ca31318a7e9cd01c6ec13e3
CONESTOGAC\jhe9837 <jerry_he2012@yahoo.com>
2018-02-06 19:10:58 -0500

    Second page basically created

--- app/src/main/java/com/example/hpark4435/a01/GetStoreActivity.java
+++ app/src/main/java/com/example/hpark4435/a01/GetStoreActivity.java
@@ -1,14 +1,29 @@
 package com.example.hpark4435.a01;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.View;
+import android.widget.Button;
 
 public class GetStoreActivity extends Activity {
+    public Button btn_ChooseItem;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_store);
+
+        btn_ChooseItem = (Button)findViewById(R.id.btn_ChooseItem);
+        btn_ChooseItem.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Intent getthirdpage = new Intent(GetStoreActivity.this, DetailActivity.class);
+                startActivity(getthirdpage);
+            }});
     }
 
 }
